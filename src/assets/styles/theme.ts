c191da0225c0ecda4effbe1a0c111c729f6e324b
Pawel Rylo <rylcior@gmail.com>
2021-11-20 19:45:48 +0100

    Add styled components

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+    colors: {
+        white: '#FFFFFF',
+        black: '#111111',
+        success: '#8FCB81',
+        error: '#FF8383',
+        warning: '#E1D888',
+    },
+    fontSize: {
+        xxl: '34px',
+        xl: '24px',
+        l: '17px',
+        m: '12px',
+        s: '11px',
+    },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './views/App';
+import App from 'views/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
